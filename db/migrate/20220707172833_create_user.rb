8e9a7f1e2124332dda5d297c2ed8368a05cc4e2b
Mark Viti <+>
2022-07-07 13:42:01 -0400

    Done.

---
+++ db/migrate/20220707172833_create_user.rb
@@ -0,0 +1,10 @@
+class CreateUser < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :password_digest
+
+      t.timestamps null: false
+    end
+  end
+end
